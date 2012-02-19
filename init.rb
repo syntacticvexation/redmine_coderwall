51e509fa4339f50edc83a785ebccb2da74e25bcc
Syntactic Vexation <syntacticvexation@gmail.com>
2012-02-20 00:42:11 +1000

    Initial commit

---
+++ init.rb
@@ -0,0 +1,9 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_coderwall do
+  name 'Redmine Coderwall plugin'
+  author 'Syntactic Vexation'
+  description 'This is a plugin for Redmine that displays your coderwall achievements on My Page'
+  version '0.0.1'
+  url 'https://github.com/syntacticvexation/redmine_coderwall'
+end
